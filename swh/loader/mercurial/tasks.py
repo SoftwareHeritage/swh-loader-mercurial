3a551fa90d3a5570f9898b6fde4e61b671d2fc0e
Antoine Lambert <anlambert@softwareheritage.org>
2022-04-27 15:21:47 +0200

    tasks: Simplify implementation and add tests for listed origins
    Recent changes in swh-scheduler add new parameters to the celery tasks
    produced from swh.scheduler.model.ListedOrigin instances.
    
    So ensure to handle any new parameters by not hardcoding the expected
    ones in task signatures.
    
    Remove unsafe use of unnamed task parameters.
    
    Add new tests checking task parameters produced from ListedOrigin
    instances do no raise error when attempting to create a hg loader.
    
    Related to T4187

--- swh/loader/mercurial/tasks.py
+++ swh/loader/mercurial/tasks.py
@@ -3,8 +3,6 @@
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
-from typing import Optional
-
 from celery import shared_task
 
 from swh.loader.core.utils import parse_visit_date
@@ -12,10 +10,14 @@ from swh.loader.core.utils import parse_visit_date
 from .loader import HgArchiveLoader, HgLoader
 
 
+def _process_kwargs(kwargs):
+    if "visit_date" in kwargs:
+        kwargs["visit_date"] = parse_visit_date(kwargs["visit_date"])
+    return kwargs
+
+
 @shared_task(name=__name__ + ".LoadMercurial")
-def load_hg(
-    *, url: str, directory: Optional[str] = None, visit_date: Optional[str] = None
-):
+def load_hg(**kwargs):
     """Mercurial repository loading
 
     Import a mercurial tarball into swh.
@@ -23,21 +25,15 @@ def load_hg(
     Args: see :func:`HgLoader` constructor.
 
     """
-    loader = HgLoader.from_configfile(
-        url=url, directory=directory, visit_date=parse_visit_date(visit_date)
-    )
+    loader = HgLoader.from_configfile(**_process_kwargs(kwargs))
     return loader.load()
 
 
 @shared_task(name=__name__ + ".LoadArchiveMercurial")
-def load_hg_from_archive(
-    *, url: str, archive_path: Optional[str] = None, visit_date: Optional[str] = None
-):
+def load_hg_from_archive(**kwargs):
     """Import a mercurial tarball into swh.
 
     Args: see :func:`HgArchiveLoader` constructor.
     """
-    loader = HgArchiveLoader.from_configfile(
-        url=url, archive_path=archive_path, visit_date=parse_visit_date(visit_date)
-    )
+    loader = HgArchiveLoader.from_configfile(**_process_kwargs(kwargs))
     return loader.load()
